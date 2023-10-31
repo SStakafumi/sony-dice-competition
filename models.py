75eec8e2a89843297e253779520ed7df9802aa09
takafumi <gtaka1213@gmail.com>
2023-11-01 02:10:49 +0900

    add UNetWrapper class

--- models.py
+++ models.py
@@ -1,6 +1,10 @@
+import math
+
 import torch.nn as nn
 from torchvision import models
-# from transforms import CLIPProcessor, CLIPModel
+
+from util.unet import UNet
+
 
 class RegNetWrapper(nn.Module):
     def __init__(self):
@@ -35,3 +39,41 @@ class ResNet18Wrapper(nn.Module):
         logit = self.resnet18(input_batch)
 
         return logit, self.head_softmax(logit)
+
+
+# UNet for image denoise
+class UNetWrapper(nn.Module):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+        self.input_batchnorm = nn.BatchNorm2d(num_features=1)
+        self.unet = UNet(**kwargs)
+        self.final = nn.Sigmoid()
+
+        self._init_weights()
+
+    def _init_weights(self):
+        init_set = {
+            nn.Conv2d,
+            nn.Conv3d,
+            nn.ConvTranspose2d,
+            nn.ConvTranspose3d,
+            nn.Linear,
+        }
+        for m in self.modules():
+            if type(m) in init_set:
+                nn.init.kaiming_normal_(
+                    m.weight.data, mode='fan_out', nonlinearity='relu', a=0
+                )
+                if m.bias is not None:
+                    fan_in, fan_out = nn.init._calculate_fan_in_and_fan_out(
+                        m.weight.data
+                    )
+                    bound = 1 / math.sqrt(fan_out)
+                    nn.init.normal_(m.bias, -bound, bound)
+
+    def forward(self, input_batch):
+        bn_output = self.input_batchnorm(input_batch)
+        un_output = self.unet(bn_output)
+        fn_output = self.final(un_output)
+        return fn_output
